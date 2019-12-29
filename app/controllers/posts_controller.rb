fbb1688a99b9ddcba9f82d5299f87e82f604ad0f
mike <michgoldennathan17@gmail.com>
2019-12-29 10:29:50 +0100

    [Feature] Add validations to post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,6 @@ class PostsController < ApplicationController
     end
 
     def post_params
-      params.require(:post).permit(:body, :user_id)
+      params.require(:post).permit(:title, :body)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 5 }
+    validates :body, presence: true, length: { minimum: 5, maximum: 255}
     belongs_to :user
 end
