73f5637c19d87ec843e837c529b961ced55c8c10
mike <michgoldennathan17@gmail.com>
2019-12-30 23:27:27 +0100

    [Feature] Include index view to show all posts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,8 +7,8 @@ module SessionsHelper
   end
   attr_writer :current_user
   def current_user
-    remember_token = User.digest(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
+    user_id = session[:user_id]
+    @current_user ||= User.find_by(id: user_id)
   end
 
   def logged_in?
