8f8d252fe4c5c272d9951c7cc0b8346831caa861
mike <michgoldennathan17@gmail.com>
2020-01-02 22:40:55 +0100

    [Fix] Correct linting errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,10 +24,9 @@ class PostsController < ApplicationController
   end
 
   def logged_in_user?
-    unless logged_in?
+    return if logged_in?
       flash[:danger] = 'Please log in.'
       redirect_to login_url
-    end
   end
 
   private
