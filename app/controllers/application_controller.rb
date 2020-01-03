4dc592714ce70ec8cb0a62c3c427d502c60897ce
MarioDena <enrique.dn@outlook.com>
2020-01-02 22:07:21 -0600

    [Fix] Moved some code out of the helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def log_out
+    current_user.update_attribute(:remember_token,
+                                  User.digest(User.new_token))
+
+    session[:user_id] = nil
+    cookies.delete(:remember_token)
+    cookies.delete(:user_id)
+    self.current_user = nil
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,9 +2,6 @@
 
 module SessionsHelper
   attr_writer :current_user
-  def log_in(user)
-    session[:user_id] = user.id
-  end
 
   def current_user
     user_id = session[:user_id]
@@ -13,27 +10,5 @@ module SessionsHelper
 
   def logged_in?
     !current_user.nil?
-  end
-
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
-
-  def log_out
-    current_user.update_attribute(:remember_token,
-                                  User.digest(User.new_token))
-
-    session[:user_id] = nil
-    cookies.delete(:remember_token)
-    cookies.delete(:user_id)
-    self.current_user = nil
   end
 end
